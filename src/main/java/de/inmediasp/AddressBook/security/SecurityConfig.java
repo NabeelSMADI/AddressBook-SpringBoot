c563f32aa19b85e2c6f141606d0999b683b3fe89
NabeelSMADI <nsmady91@gmail.com>
2020-01-21 10:49:12 +0100

    securityjwt

--- src/main/java/de/inmediasp/AddressBook/security/SecurityConfig.java
+++ src/main/java/de/inmediasp/AddressBook/security/SecurityConfig.java
@@ -1,29 +1,63 @@
 package de.inmediasp.AddressBook.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	
-		 http
-         .httpBasic()
-         .and()
-         .authorizeRequests()
-         .antMatchers(HttpMethod.DELETE, "/api/AddressBook/**").hasRole("USER")
-         .and()
-         .csrf().disable()
-         .formLogin().disable();
-	}
 
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("USER");
+	private AddressBookUserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		// Entry points
+		http.authorizeRequests()
+				.antMatchers("/api/AddressBook/**").permitAll()
+				.antMatchers("/users/signin").permitAll()
+				.antMatchers("/users/**").permitAll()
+				// Disallow everything else..
+				.anyRequest().authenticated();
+
+		// Disable CSRF (cross site request forgery)
+		http.csrf().disable();
+
+		// No session will be created or used by spring security
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(new JwtTokenFilter(userDetailsService), UsernamePasswordAuthenticationFilter.class);
 	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// Allow swagger to be accessed without authentication
+		web.ignoring().antMatchers("/v2/api-docs")
+				.antMatchers("/configuration/**")
+				.antMatchers("/webjars/**")
+				.antMatchers("/public");
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12);
+	}
+
 }
